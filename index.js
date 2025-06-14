43689bc5cb2edc65c9c203ae50a1b0bcb4df6bea
л→в <belobrov.lev@gmail.com>
2025-06-14 19:46:56 +0200

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name             Remove ads from Mangalib
+// @name:ru          Удалить рекламу с главной Мангалиба
+// @version          1.3
+// @description      Deletes the slider with the advertisement on the main page of the website.
+// @description:ru   Удаляет слайдер с рекламой на главной странице сайта.
+// @match            https://mangalib.me/
+// @run-at           document-idle
+// @grant            none
+// ==/UserScript==
+
+const PARENT_SELECTOR = '#app > div.page > div.container.container_offset.fade > div';
+const TARGET_SELECTOR = 'div.section[data-home-block="slider"]';
+
+(function() {
+    'use strict';
+
+    function removeTarget() {
+        const parent = document.querySelector(PARENT_SELECTOR);
+        const target = parent?.querySelector(TARGET_SELECTOR);
+        if (target) {
+            target.remove();
+            alert('true!');
+            return true;
+        }
+        return false;
+    }
+
+    if (!removeTarget()) {
+        alert('set observer');
+        const observer = new MutationObserver(() => {
+            if (removeTarget()) observer.disconnect();
+        });
+        observer.observe(document, { childList: true, subtree: true });
+    }
+})();
